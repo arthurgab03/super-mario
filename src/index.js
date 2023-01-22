ab601e5e80c9398b7d7048639220781c54eb625c
Arthur Gabriel <arthurgabriel120703@gmail.com>
2023-01-22 18:21:06 -0300

    últimas alterações

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const botaoTrailer = document.querySelector(".botao-trailer");
-const modal = document.querySelector(".modal");
+const popup = document.querySelector(".popup");
 const video = document.getElementById("video");
-const fecharModal = document.querySelector(".fechar-modal");
+const fecharPopup = document.querySelector(".close-popup");
 const linkVideo =  video.src; 
 /* a const video pegou o id video que tem no html, ou seja, essa const tem as informações do #video, significa que podemos extrair essas informações, como o src por exemplo, sabendo disso, criei uma const linkVideo que recebe a src do #video e isso será muito útil para funções abaixo. */
 
 
 function alternar () {
-    modal.classList.toggle("aberto"); /* aqui adicionamos a classe .aberto que faz com que o pop-up do vídeo apareça, neste caso, o toggle serve para verificar se esta classe está presente em algum elemento, se não estiver, ela será adicionada, se estiver, ela será removida, assim podemos utilizar esta função como uma forma limpa de fechar e abrir o pop-up */
+    popup.classList.toggle("aberto"); /* aqui adicionamos a classe .aberto que faz com que o pop-up do vídeo apareça, neste caso, o toggle serve para verificar se esta classe está presente em algum elemento, se não estiver, ela será adicionada, se estiver, ela será removida, assim podemos utilizar esta função como uma forma limpa de fechar e abrir o pop-up */
 
 }
 
@@ -18,7 +18,7 @@ botaoTrailer.addEventListener("click", () => {
 });
 
 
- fecharModal.addEventListener("click", () => {
+ fecharPopup.addEventListener("click", () => {
     alternar(); // fechando o pop-up 
     video.setAttribute("src", ""); //removendo o src do vídeo para que não ocorra vazamento de áudio após fechar o pop-up 
 });
